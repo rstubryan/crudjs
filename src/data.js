c7aa44c4620ac0361d87429477c4180d89624a17
rstubryan <restu.bumi_ti21@nusaputra.ac.id>
2023-07-21 11:09:34 +0700

    wip penambahan form create dan show button dihapus

--- src/data.js
+++ src/data.js
@@ -1,24 +1,47 @@
 function createData() {
-   const xhttp = new XMLHttpRequest();
-   window.location.href='formcreate.html';
+   const formData = new FormData(document.getElementById("formInput"));
+
+   // Kirim data menggunakan AJAX
+   fetch("https://iussi.serveo.net/belajar-php/mahasiswa/mahasiswa.php", {
+      method: "POST",
+      body: formData,
+   })
+      .then((response) => response.json())
+      .then((data) => {
+         // Tangani respon dari server (tambah.php) jika diperlukan
+         console.log(data);
+         alert(data.message);
+         document.getElementById("formInput").reset();
+      })
+      .catch((error) => {
+         console.error("Error:", error);
+      });
 }
 
 function loadData() {
    const xhttp = new XMLHttpRequest();
+   document.getElementById("load").innerHTML;
    xhttp.onload = function () {
       const response = JSON.parse(this.responseText);
       console.log(response);
 
       let tableData = "";
-      response.forEach((item) => {
-         tableData += `<tr class="border border-slate-700 bg-slate-500"><td class="border border-slate-700">${item.id}</td> <td class="border border-slate-700">${item.nama}</td> <td class="border border-slate-700">${item.nim}</td> <td class="border border-slate-700">${item.jurusan}</td><td><a href="#">Update</a> | <a href="#">Delete</a></td></tr>`;
-      });
 
+      response.forEach((item) => {
+         tableData += `<tr class="border border-slate-700 bg-slate-500">
+         <td class="border border-slate-700">${item.id}</td> 
+         <td class="border border-slate-700">${item.nama}</td> 
+         <td class="border border-slate-700">${item.nim}</td> 
+         <td class="border border-slate-700">${item.jurusan}</td>
+         <td><a href="#">Update</a> | 
+         <a href="#">Delete</a></td></tr>`;
+      });
       document.getElementById("table_data").innerHTML += tableData;
+      document.getElementById("load").remove();
    };
    xhttp.open(
       "GET",
-      "https://comparo.serveo.net/belajar-php/mahasiswa/mahasiswa.php"
+      "https://iussi.serveo.net/belajar-php/mahasiswa/mahasiswa.php"
    ); // data server perlu diganti koneksinya setiap generate
    xhttp.send();
 }
--- src/form.js
+++ src/form.js
@@ -4,24 +4,24 @@ function generateForm() {
           <p class="text-center py-1">Data Mahasiswa</p>
  
           <div class="mx-auto block text-center py-4 container w-96">
-             <form id="crud-form">
+             <form id="formInput">
                 <div>
-                   <label for="nama-input" class="block mb-2 text-sm font-bold text-gray-900 py-2">Nama</label>
-                   <input type="text" id="nama-input" class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500" />
+                   <label for="nama" class="block mb-2 text-sm font-bold text-gray-900 py-2">Nama</label>
+                   <input type="text" id="nama" name="nama" class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500" />
                 </div>
  
                 <div>
-                   <label for="nim-input" class="block mb-2 text-sm font-bold text-gray-900 py-2">NIM</label>
-                   <input type="text" id="nim-input" class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500" />
+                   <label for="nim" class="block mb-2 text-sm font-bold text-gray-900 py-2">NIM</label>
+                   <input type="text" id="nim" name="nim" class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500" />
                 </div>
  
                 <div>
-                   <label for="jurusan-input" class="block mb-2 text-sm font-bold text-gray-900 py-2">Jurusan</label>
-                   <input type="text" id="jurusan-input" class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500" />
+                   <label for="jurusan" class="block mb-2 text-sm font-bold text-gray-900 py-2">Jurusan</label>
+                   <input type="text" id="jurusan" name="jurusan" class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500" />
                 </div>
  
                 <div class="py-4">
-                   <button type="submit" class="bg-slate-800 hover:bg-slate-900 active:bg-slate-700 focus:outline-none focus:ring focus:ring-slate-300 border rounded-full px-4 py-2 mx-auto block" onClick="alert("Yakin?")">Save changes</button>
+                   <button type="button" class="bg-slate-800 hover:bg-slate-900 active:bg-slate-700 focus:outline-none focus:ring focus:ring-slate-300 border rounded-full px-4 py-2 mx-auto block" onClick="createData()">Save changes</button>
                 </div>
              </form>
           </div>
